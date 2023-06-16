8e94ff86219b70b10e78fc41485fbeed457c0202
nvat <nvatuan2000@gmail.com>
2023-06-16 21:59:50 +0700

    File Storage service

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import request, Flask, send_file, jsonify
+app = Flask(__name__)
+
+from storage import FileStorage
+FS = FileStorage('./data')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'image' not in request.files:
+        return jsonify({
+            'message': 'Missing required image',
+        }), 400
+
+    image = request.files['image']
+    url = FS.save_file(image)
+    return jsonify({
+        'message': 'Image uploaded successfully!',
+        'data': {
+            'file_name': url
+        }
+    })
+
+@app.route('/image/<path:filename>')
+def get_image(filename):
+    if not FS.does_file_name_exist(filename):
+        return jsonify({ 'message': 'Image not found', }), 404
+
+    file_url = FS.get_file_url(filename)
+    try:
+        return send_file(file_url)
+    except:
+        return jsonify({ 'message': 'Cannot get image', }), 500
+
+
+@app.route('/image/<path:filename>', methods=['DELETE'])
+def delete_file(filename):
+    if not FS.does_file_name_exist(filename):
+        return jsonify({ 'message': 'Image not found', }), 404
+
+    if FS.delete_file(filename):
+        return jsonify({ 'message': 'Ok', })
+
+    return jsonify({ 'message': 'Cannot delete image', }), 500
+
+if __name__ == '__main__':
+    app.run()
---
+++ storage.py
@@ -0,0 +1,40 @@
+import os
+import uuid
+
+"""
+    File Storage class
+"""
+class FileStorage:
+    def __init__(self, store_url):
+        super().__init__()
+        self.store_url = store_url
+
+    def __get_path_to_file(self, fname):
+        return os.path.join(self.store_url, fname)
+    
+    def __generate_name_from_file(self, file):
+        random_uuid = uuid.uuid4()
+        random_uuid_str = str(random_uuid)[:13]
+        filename = file.filename
+        return f'{random_uuid_str}-{filename}'
+
+    def save_file(self, file):
+        file_name = self.__generate_name_from_file(file)
+        file_path = self.__get_path_to_file(file_name)
+        file.save(file_path)
+        return file_name
+
+    def get_file_url(self, file_name):
+        return self.__get_path_to_file(file_name)
+
+    def does_file_name_exist(self, file_name):
+        file_url = self.__get_path_to_file(file_name)
+        return os.path.exists(file_url)
+
+    def delete_file(self, file_name):
+        file_url = self.__get_path_to_file(file_name)
+        try:
+            os.remove(file_url)
+            return True
+        except:
+            return False
