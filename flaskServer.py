0248693a7f56c4c08522d12de003fc70aa5696e4
qhuy <huy.tran@gotecq.com>
2023-06-17 02:20:08 +0700

    fix: read image from storage server

--- flaskServer.py
+++ flaskServer.py
@@ -5,6 +5,8 @@ from flask_cors import CORS, cross_origin
 from timeit import default_timer as timer
 
 import json
+
+import requests
 from OCR_Interface import OCR_Interface
 
 ocr_interface = OCR_Interface()
@@ -12,15 +14,20 @@ ocr_interface = OCR_Interface()
 app = Flask(__name__)
 
 cors = CORS(app)
+from PIL import Image
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+def getImagefromExpress(imageName):
+    url = f'http://localhost:1606/image/{imageName}'
+    img = Image.open(requests.get(url, stream=True).raw)
+    return img
 
 @app.route("/", methods = ['POST'])
 @cross_origin()
-
 def sendImage():
     data = request.get_json()
     message = data.get("message")
-    content = data.get("content")
+    fileName = data.get("file_name")
 
     print("this message", message)
 
@@ -34,8 +41,9 @@ def sendImage():
 
     if (message == "convert image to text"):
         start = timer()
-        inputImagePath = content
+        inputImagePath = getImagefromExpress(fileName)
         ocrOutput = ocr_interface.imageToText(inputImagePath)
+        # print('@@@@@@@@@@@@@@', inputImagePath)
         end = timer()
         print(end - start)
         return json.dumps(ocrOutput.strip())
